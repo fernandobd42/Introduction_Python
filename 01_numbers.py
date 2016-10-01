a14ac244f543acd5b7a81c36b88d0f44f835f7a8
fernandobd43 <fernandolucasbd@gmail.com>
2016-10-01 15:31:09 -0400

    Numbers

---
+++ 01_numbers.py
@@ -0,0 +1,13 @@
+x = 1 #x recebe 1
+#print irá printar(mostrar) o valor desejado;
+print(x) # resultado: 1
+print(x + 4) # é possível somar uma variável com um número, desde que a variável tenha um valor definido - resultado: 5
+print(2 * 2) #um asterisco é usado para multiplicar - resultado: 4
+print(3 ** 3) #dois asterisco é usado para elevar a potência - resultado: 27
+print(5 / 2) #divisão com uma barra é usado para retornar tipo flutuante - resultado = 2.5
+print(5 // 2) #divisão com duas barras é usado para retorna tipo inteiro - resultado = 2;
+print(5 % 2) #modulo é usado para retornar o resto da divisão - resultado = 1;
+
+#OBS: o uso de '=' atribui um valor a uma variável, já o uso de '==' compara os valores;
+y = 1 #1 é atribuído para y;
+y == 1 #y é igual a 1?
